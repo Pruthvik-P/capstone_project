adc7310db7a2b316211c7b193f5ace1f40136d30
Pruthvik_P <Pruthvik6666@gmail.com>
2024-05-07 15:15:05 +0530

    project1

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import reportWebVitals from './reportWebVitals'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -10,4 +10,3 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   </React.StrictMode>,
   
 )
-reportWebVitals();
